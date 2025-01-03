a5f92913c4e2d30ad75191ae3fa4eb22a43b01fa
BensonRajaB <bensonraja.b@teamsybergate.com>
2025-01-03 10:28:20 +0530

    dispute view dto

--- src/services/disputes/dto/GetDisputeForViewDto.ts
+++ src/services/disputes/dto/GetDisputeForViewDto.ts
@@ -1,7 +1,8 @@
 import { DisputeDto } from "./DisputeDto";
+
 export interface GetDisputeForViewDto{
     Dispute:DisputeDto;
-    Id:number;
+    id:Number;
     SupplementarySummaryDisplayProperty:string;
     SupplierRejectionCode:string;
     SupplierCode:string;
@@ -10,5 +11,8 @@ export interface GetDisputeForViewDto{
     SupplierMail:string;
     BuyerName:string;
     FromMail:string;
-
+    AccoutantMail:string;
+    AccoutantName:string;
+    SupplierName:string;
+    AppURL:string;    
 }
