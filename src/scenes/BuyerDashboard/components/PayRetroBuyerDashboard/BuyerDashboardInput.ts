c931cb42238525c66dae745923468484396a2844
Dhivakar Perumalsamy <122018210+dhivakar-syber@users.noreply.github.com>
2025-01-06 19:12:14 +0530

    Merge

--- src/scenes/BuyerDashboard/components/PayRetroBuyerDashboard/BuyerDashboardInput.ts
+++ src/scenes/BuyerDashboard/components/PayRetroBuyerDashboard/BuyerDashboardInput.ts
@@ -2,9 +2,9 @@ export interface BuyerDashboardInput {
     Supplierids:number[];
       Buyerid: number;
       Partids:number[];
-      invoicetype:number; 
-      Date:Date|null;
-      Document:string | null;
+        invoicetype:number; 
+        Date:Date|null;
+        Document:string | null;
   
     }
     
