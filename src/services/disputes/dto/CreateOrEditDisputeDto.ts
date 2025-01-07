08a0884a43eb89854ae42258204414b55e06709d
Mohamed Sakkeel R <mohamedsakkeel.r@teamsybergate.com>
2025-01-07 19:53:58 +0530

    werfw

--- src/services/disputes/dto/CreateOrEditDisputeDto.ts
+++ src/services/disputes/dto/CreateOrEditDisputeDto.ts
@@ -11,7 +11,5 @@ export interface CreateOrEditDisputeDto
     SupplierRejectionId:number;
     SupplierId:number;
     BuyerId:number;
-    SupplierRejection:string;
-    SupplierName:string;
-    BuyerName:string;
+
 }
